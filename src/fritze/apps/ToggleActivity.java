543537ec69c90688f4e34c36d49011315842956f
mfritze <mfritze@ualberta.ca>
2015-04-28 16:35:30 -0600

    Updating image accordingly

--- src/fritze/apps/ToggleActivity.java
+++ src/fritze/apps/ToggleActivity.java
@@ -7,70 +7,28 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Toast;
 
-@SuppressWarnings("deprecation")
 public class ToggleActivity extends FragmentActivity {
-	// Based on http://stackoverflow.com/questions/6068803/how-to-turn-on-camera-flash-light-programmatically-in-android April 24th 2015
-	static Camera camera;
-	
+
+	private static FlashController flashController = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.toggle_layout);
+		flashController = new FlashController(this);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		// TODO method to update the icon and place mark
-		openCamera();
-		
+		flashController.updateImage();
 	}
 	
 	public void toggleFlashlight(View v){
-		openCamera();
-		try{
-			Parameters p = camera.getParameters();
-			if(p.getFlashMode().equals(Parameters.FLASH_MODE_TORCH)){
-				flashLightToggleOff(p);
-			}else if (p.getFlashMode().equals(Parameters.FLASH_MODE_OFF)){
-				flashLightToggleOn(p);
-			} else {
-				Toast.makeText(this, p.getFlashMode(), Toast.LENGTH_SHORT).show();
-			}
+		flashController.toggleFlashLight();
+	}
+	
 
-		}catch(RuntimeException e){
-			Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
-			e.printStackTrace();
-			if(camera != null){
-				camera.release();
-				camera = null;
-			}
-		}
-
-	}
-	
-	private void flashLightToggleOff(Parameters par){
-		Toast.makeText(this, "Flash is on, turning off", Toast.LENGTH_SHORT).show();
-		//TODO update image
-		camera.stopPreview();
-		camera.release();
-		camera = null;
-	}
-	
-	private void flashLightToggleOn(Parameters par){
-		Toast.makeText(this, "Flash is off, turning on", Toast.LENGTH_SHORT).show();
-		par.setFlashMode(Parameters.FLASH_MODE_TORCH);
-		camera.setParameters(par);
-		camera.startPreview();
-	}
-	
-	private void openCamera(){
-		if(camera == null){
-			camera = Camera.open();
-			//camera.unclock();
-		}
-	}
-	
 	
 }
