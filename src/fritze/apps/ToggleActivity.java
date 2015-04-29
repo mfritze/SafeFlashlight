4e7ddc951c96b4e7b1065f0d30ce8ec0e5bfd635
mfritze <mfritze@ualberta.ca>
2015-04-28 23:16:51 -0600

    Starting to add notifaction support

--- src/fritze/apps/ToggleActivity.java
+++ src/fritze/apps/ToggleActivity.java
@@ -1,11 +1,14 @@
 package fritze.apps;
 
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 
 public class ToggleActivity extends FragmentActivity {
-
+	private NotificationCompat.Builder builder;
+	private final int NOTIFICATIONID = 2147483647; // 2**31 - 1 mersene prime 
 	private static FlashController flashController = null;
 	
 	@Override
@@ -13,20 +16,21 @@ public class ToggleActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.toggle_layout);
 		flashController = new FlashController(this);
+		
+		builder = new NotificationCompat.Builder(this)
+		.setSmallIcon(R.drawable.temp_notification)
+		.setContentTitle("Foxlight")
+		.setContentText("Flashlight");
 	}
+
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+		flashController.openCamera();
 		flashController.updateImage();
 	}
-
 	
-	@Override
-	protected void onPause(){
-		super.onPause();
-		flashController.pause();
-	}
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
@@ -35,8 +39,13 @@ public class ToggleActivity extends FragmentActivity {
 
 	public void toggleFlashlight(View v){
 		flashController.toggleFlashLight();
+		launchNotification();
 	}
 	
-
+	public void launchNotification(){
+		// TODO some check that it's not already there? or make a take down if it is? that way you dont have to return it
+		NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		manager.notify(NOTIFICATIONID, builder.build());
+	}
 	
 }
